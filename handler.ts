e13eddcb674dc8de2404d591ce1ac50325e002f4
mark olsen <olsen.mark@gmail.com>
2018-02-06 08:35:12 +1300

    add express and serverless-http and use express for routing

--- handler.ts
+++ handler.ts
@@ -1,13 +1,10 @@
 import { APIGatewayEvent, Callback, Context, Handler } from 'aws-lambda';
+import serverless = require('serverless-http');
+import express = require('express')
+const app = express()
 
-export const hello: Handler = (event: APIGatewayEvent, context: Context, cb: Callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
+app.get('/hello', (req, res)=>{
+  res.send("hello typescript world");
+})
 
-  cb(null, response);
-}
+export const hello: Handler = serverless(app);
