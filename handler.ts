9872590d399e3040cc4f5cd95e922b8b91f906c2
mark olsen <olsen.mark@gmail.com>
2018-02-07 21:25:49 +1300

    add dynamodb write functionality. Oh i had some troublewith this with a region being required and also trying to store the key of id as a number rather than a string - this was the error Type mismatch for key id expected: S actual: N

--- handler.ts
+++ handler.ts
@@ -1,14 +1,44 @@
 import { APIGatewayEvent, Callback, Context, Handler } from 'aws-lambda';
 import serverless = require('serverless-http');
 import express = require('express')
-const app = express()
+import AWS = require('aws-sdk'); // eslint-disable-line import/no-extraneous-dependencies
+import {DocumentClient} from "aws-sdk/lib/dynamodb/document_client";
 
-app.get('/hello', (req, res)=>{
+AWS.config.update({region: "us-east-1"});
+const dynamoDb: DocumentClient = new AWS.DynamoDB.DocumentClient();
+
+const app = express();
+
+app.get('/hello', (req, res)=>{ 
   res.send("hello typescript world");
 })
 
 app.get('/goodbye', (req, res)=>{
   res.send("goodbye typescript world");
+})
+
+app.get("/write", (req, res) => {
+  console.log(JSON.stringify(process.env))
+  const params = {
+    TableName: process.env.DYNAMODB_TABLE,
+   // TableName: process.env.DYNAMODB_TABLE,
+    Item: {
+      id: "123"+Math.random(),
+      text: "4 score ...",
+      checked: false,
+      createdAt: new Date().toISOString(),
+      updatedAt: new Date().toISOString(),
+    },
+  };
+  dynamoDb.put(params, (error) => {
+    if (error){
+    console.log(error);
+  } else {
+    console.log(JSON.stringify(params));
+    res.send(JSON.stringify(params))
+  }
+  })
+  
 })
 
 export const api: Handler = serverless(app);
